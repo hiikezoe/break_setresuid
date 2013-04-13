0c8aa36eac65087729734e62f2c3b28a652f81cb
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2013-04-13 17:51:18 +0900

    IS17SH

--- main.c
+++ main.c
@@ -37,6 +37,7 @@ static supported_device supported_devices[] = {
   { "F-03D",            "V24R33Cc",   0xc00e83ce },
   { "F-12C",            "V21"     ,   0xc00e5ad2 },
   { "IS11N",            "GRJ90"   ,   0xc00f0a46 },
+  { "IS17SH",           "01.00.03" ,  0xc01b82e6 },
   { "URBANO PROGRESSO", "010.0.3000", 0xC0176D82 },
 };
 
